69c9fd27bf9c0df2f0eec273f98961bca35fd464
Todd Vlk <45958861+Todd-ECU@users.noreply.github.com>
2020-06-13 00:52:17 -0400

    compute pay function assignment complete

--- computepay.py
+++ computepay.py
@@ -1,18 +1,41 @@
 
 '''
 4.6 Write a program to prompt the user for hours and rate per hour using input to compute gross pay. 
-Pay should be the normal rate for hours up to 40 and time-and-a-half for the hourly rate for all hours worked above 40 hours. 
-Put the logic to do the computation of pay in a function called computepay() and use the function to do the computation. 
-The function should return a value. Use 45 hours and a rate of 10.50 per hour to test the program (the pay should be 498.75). 
+Pay should be the normal rate for hours up to 40 and time-and-a-half for the hourly rate for 
+all hours worked above 40 hours. 
+Put the logic to do the computation of pay in a function 
+called computepay() and use the function to do the computation. 
+The function should return a value. Use 45 hours and a rate of 10.50 per hour 
+to test the program (the pay should be 498.75). 
 You should use input to read a string and float() to convert the string to a number. 
 Do not worry about error checking the user input unless you want to - you can assume the user types numbers properly. 
 Do not name your variable sum or use the sum() function.
 
 '''
 
-def computepay(h,r):
-    return 42.37
+hrs = input("Enter Hours: ")
+rate = input("Enter Rate Per Hour: ")
+try:
+    h = float(hrs)
+    r = float(rate)
+except:
+    print("***Error, please enter numeric input***")
+    raise SystemExit()
 
-hrs = input("Enter Hours:")
-p = computepay(10,20)
+
+def computepay(h,r):
+    otr = r*1.5
+    oth = h - 40    
+    regpay = h * r
+    otpay = oth * otr
+    bh = h - oth
+    basepay = bh * r
+
+    if h <= 40:
+        return regpay
+    else:    
+        return basepay + otpay
+    
+
+p = computepay(h,r)
 print("Pay",p)
