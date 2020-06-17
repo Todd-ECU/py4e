98dbedfac980890618de65aeaee16238522f1cb6
Todd Vlk <45958861+Todd-ECU@users.noreply.github.com>
2020-06-16 23:43:53 -0400

    add text file

--- Assignment7.2.py
+++ Assignment7.2.py
@@ -4,6 +4,9 @@
  looking for lines of the form:
 X-DSPAM-Confidence:    0.8475
 Count these lines and extract the 
-floating point values from each of the lines and compute the average of those values and produce an output as shown below. Do not use the sum() function or a variable named sum in your solution.
+floating point values from each of the lines and compute 
+the average of those values and produce an output as shown below. 
+Do not use the sum() function or a variable named sum in your solution.
 You can download the sample data 
 at http://www.py4e.com/code3/mbox-short.txt when you are testing below enter mbox-short.txt as the file name.
+'''
