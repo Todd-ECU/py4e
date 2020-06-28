9c8651b7ad4532c8f9e3b103e94ed7d7de0651b1
Todd Vlk <45958861+Todd-ECU@users.noreply.github.com>
2020-06-28 01:23:29 -0400

    8.4 complete and submitted

--- assignment_8.4.py
+++ assignment_8.4.py
@@ -9,9 +9,10 @@ fname = input("Enter file name: ")
 fh = open(fname)
 lst = list()
 for line in fh:
-   line = line.rstrip()
-   words = line.split()
-
-
-   print(words)
+   for word in line.split():
+      if word not in lst:
+          lst.append(word)
+          lst.sort()
+print(lst)
    
+ 
