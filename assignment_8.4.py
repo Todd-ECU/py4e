9c1c66d35d4ae6aebfbfdfd16ae94c97e00d9b79
Todd Vlk <45958861+Todd-ECU@users.noreply.github.com>
2020-06-28 00:19:09 -0400

    commit your plans

--- assignment_8.4.py
+++ assignment_8.4.py
@@ -9,4 +9,9 @@ fname = input("Enter file name: ")
 fh = open(fname)
 lst = list()
 for line in fh:
-   print(line.rstrip())
+   line = line.rstrip()
+   words = line.split()
+
+
+   print(words)
+   
