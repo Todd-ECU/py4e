27935c983fcc2196ae480cff83add25ff33892a1
Todd Vlk <45958861+Todd-ECU@users.noreply.github.com>
2020-07-23 22:49:33 -0400

    just another commit

---
+++ Assignment10.2.py
@@ -0,0 +1,16 @@
+fname = input("Enter file name: ")
+if len(fname) < 1 : fname = "my.txt"
+hand = open(fname)
+
+di = dict()
+for lin in hand:
+    lin = lin.rstrip()
+    wds = lin.split()
+    for w in wds:
+        # idiom: retrieve/create/update counter
+        di[w] = di.get(w,0) + 1
+
+#print(di)  
+
+x = sorted(di.items())
+print(x[:10])
